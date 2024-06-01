710549799a5ffde708662cb3db6d9ff4d8ec3d3a
valentinamuarcos <valentinama@unicauca.edu.co>
2024-06-01 15:43:04 -0500

    Create Cactus.h

---
+++ Cactus.h
@@ -0,0 +1,15 @@
+#ifndef Cactus_h
+#define Cactus_h
+#include "Arduino.h"
+
+class Cactus {
+  private:    
+    int x;         
+
+  public:
+    Cactus(int posicionInicial);
+    void mover();
+    int obtenerPosicion();    
+};
+
+#endif
