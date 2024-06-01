f03f384c5da390101be27230278c3bd2150aa262
valentinamuarcos <valentinama@unicauca.edu.co>
2024-06-01 15:43:35 -0500

    Create Cactus.cpp

---
+++ Cactus.cpp
@@ -0,0 +1,18 @@
+#include "Cactus.h"
+
+Cactus::Cactus(int posicionInicial) {  
+  x = posicionInicial;  
+}
+
+void Cactus::mover() {   
+  if(x > 0) {
+    x = x - 1;
+  }
+  else {
+    x = 16;
+  }  
+}
+
+int Cactus::obtenerPosicion() {
+  return x;
+}
